5a5f551e6e15266695f781814b4bf4f8f501aadd
Aryamaan Kunwar <aryamaan3@gmail.com>
2021-02-24 00:42:49 +0100

    get put delete for video api

--- app.py
+++ app.py
@@ -4,13 +4,41 @@ from flask_restful import Resource, Api, abort, reqparse
 app = Flask(__name__)
 api = Api(app)
 
+video_put_args = reqparse.RequestParser()
+video_put_args.add_argument("name", type=str, help="Name of the video is required", required=True)
+video_put_args.add_argument("views", type=int, help="Views of video")
+video_put_args.add_argument("likes", type=int, help="Likes on the video")
 
-class Test(Resource):
-    def get(self):
-        return "Hello World"
+videos = {}
 
 
-api.add_resource(Test, "/helloworld")
+def abort_if_video_id_doesnt_exist(video_id):
+    if video_id not in videos:
+        abort(404, message="Video id not valid...")
+
+def abort_if_video_exists(video_id):
+    if video_id in videos:
+        abort(409, message="Video already exists with that id...")
+
+
+class Video(Resource):
+    def get(self, video_id):
+        abort_if_video_id_doesnt_exist(video_id)
+        return videos[video_id]
+
+    def put(self, video_id):
+        abort_if_video_exists(video_id)
+        args = video_put_args.parse_args()
+        videos[video_id] = args
+        return videos[video_id], 201  # created
+
+    def delete(self, video_id):
+        abort_if_video_id_doesnt_exist(video_id)
+        del videos[video_id]
+        return '', 204
+
+
+api.add_resource(Video, "/video/<int:video_id>")
 
 if __name__ == '__main__':
     app.run(debug=True)
--- test.py
+++ test.py
@@ -2,5 +2,11 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-response = requests.get(BASE + "helloworld")
+response = requests.put(BASE + "video/1", {"name": "day at zoo"})
 print(response.json())
+input()
+response = requests.get(BASE + "video/6")
+print(response.json())
+input()
+response = requests.delete(BASE + "video/1")
+print(response)
