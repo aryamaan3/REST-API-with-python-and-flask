e59b40648a10093c94fc9a724c8ec2f3e79fb6f0
Aryamaan Kunwar <aryamaan3@gmail.com>
2021-02-24 14:18:59 +0100

    better test.py for interactivity

--- test.py
+++ test.py
@@ -2,8 +2,23 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-response = requests.get(BASE + "video/1")
+data = [{"name": "day at zoo", "views": 100000, "likes": 800},
+        {"name": "birthday song", "views": 3000, "likes": 20},
+        {"name": "python eats otter", "views": 200, "likes": 5}]
+
+print("je fais un put pour ajouter 3 videos au db")
+
+for i in range(len(data)):
+    response = requests.put(BASE + "video/" + str(i), data[i])
+    print(response.json())
+
+print("get video : ")
+index = input("0, 1 ou 2\n")
+response = requests.get(BASE + "video/" + index)
 print(response.json())
-input()
-response = requests.patch(BASE + "video/1", {"views": 100})
+
+print("update, changer la valeur")
+key = input("name, views ou likes\n")
+value = input("nouvelle valeur : ")
+response = requests.patch(BASE + "video/" + index, {key: value})
 print(response.json())
